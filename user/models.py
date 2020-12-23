0a1329f269538b76e9e05fa14d0eda31d629665b
afifsahar <67785781+afifsahar@users.noreply.github.com>
2020-12-23 08:40:25 +0700

    requirements2.txt

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
+import PIL
 from PIL import Image, ImageDraw
 import qrcode
 from io import BytesIO
